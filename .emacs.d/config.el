32aa7df319b3d245f8200383ff7e3f82b3a79f94
Zoey <zoey.spessanha@zeetech.io>
2021-01-28 12:28:37 -0300

    added neotree config

--- .emacs.d/config.el
+++ .emacs.d/config.el
@@ -614,5 +614,36 @@ server getting expensively restarted when reverting buffers."
 
   (add-hook 'haskell-mode-hook 'dante-mode))
 
+;; Neotree------------------------------------------------------
+(defun my-neotree-project-dir-toggle ()
+  "Open NeoTree using the project root, using projectile, find-file-in-project, or the current buffer directory."
+  (interactive)
+  (require 'neotree)
+  (let* ((filepath (buffer-file-name))
+         (project-dir
+          (with-demoted-errors
+              (cond
+               ((featurep 'projectile)
+                (projectile-project-root))
+               ((featurep 'find-file-in-project)
+                (ffip-project-root))
+               (t ;; Fall back to version control root.
+                (if filepath
+                    (vc-call-backend
+                     (vc-responsible-backend filepath) 'root filepath)
+                  nil)))))
+         (neo-smart-open t))
+
+    (if (and (fboundp 'neo-global--window-exists-p)
+             (neo-global--window-exists-p))
+        (neotree-hide)
+      (neotree-show)
+      (when project-dir
+        (neotree-dir project-dir))
+      (when filepath
+        (neotree-find filepath)))))
+
+(define-key global-map (kbd "C-c t") 'my-neotree-project-dir-toggle)
+
 (provide 'config)
 ;;; config.el ends here
