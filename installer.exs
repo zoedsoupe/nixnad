05cc0a19bc2c91371c160648c8d1d96cd9774435
Zoey <zoey.spessanha@zeetech.io>
2021-02-23 10:28:43 -0400

    installer v2 finished

--- installer.exs
+++ installer.exs
@@ -4,124 +4,182 @@
 # |_| |_| |_|\__,_|___/ .__/
 #                    |_|
 
+defmodule Helpers do
+  @config_path Path.expand("~/.config")
+  @home_dir Path.expand("~")
+
+  def skip(config), do: "Skipping #{config}..." |> Colors.info()
+
+  def link_to_home(existing, config), do: File.ln_s!(existing, @home_dir <> "/#{config}")
+
+  def link_to_dot_config(existing, config),
+    do: File.ln_s!(existing, @config_path <> "/#{config}")
+
+  def root_config(commands) do
+    "Please, insert your sudo pass:"
+    |> Colors.warn()
+
+    System.cmd("sudo", ["-A"] ++ commands, into: password_get())
+  end
+
+  def link_if_exist(test_path, config, config_path, link_fun) when is_binary(config_path) do
+    if File.exists?(test_path) do
+      skip(config)
+    else
+      link_fun.(config_path, config)
+
+      "Linked #{config}!\n"
+      |> Colors.success()
+    end
+  end
+
+  def link_if_exist(test_path, config, link_fun, commands: commands) do
+    if File.exists?(test_path) do
+      skip(config)
+    else
+      link_fun.(commands)
+
+      "Linked #{config}!\n"
+      |> Colors.success()
+    end
+  end
+
+  defp password_get() do
+    pid = spawn_link(fn -> loop() end)
+    ref = make_ref()
+    value = IO.read(:stdio, :line)
+
+    send(pid, {:done, self(), ref})
+    receive do: ({:done, ^pid, ^ref} -> :ok)
+
+    value
+  end
+
+  defp loop() do
+    receive do
+      {:done, parent, ref} ->
+        send(parent, {:done, self(), ref})
+        IO.write(:standard_error, "\e[2K\r")
+    after
+      1 ->
+        IO.write(:standard_error, "\e[2K\r ")
+        loop()
+    end
+  end
+end
+
+defmodule Colors do
+  import IO.ANSI, only: [format: 2]
+
+  def success(task) do
+    [:green, :bright, "DONE: ", :green, task]
+    |> format(true)
+    |> IO.puts()
+  end
+
+  def error(reason) do
+    [:red, :bright, "ERROR: ", :red, reason]
+    |> format(true)
+    |> IO.puts()
+  end
+
+  def warn(info) do
+    [:yellow, :bright, "\nWARNING: ", :yellow, info]
+    |> format(true)
+    |> IO.puts()
+  end
+
+  def info(desc) do
+    [:blue, :bright, "INFO: ", :blue, desc]
+    |> format(true)
+    |> IO.puts()
+  end
+end
+
 defmodule Installer do
   @moduledoc """
   Run this Elixir script to automatically link all these configs!
   """
 
   import Colors
+  import Helpers
 
   @config_path Path.expand("~/.config")
   @home_dir Path.expand("~")
 
-  @ignore [~r(\.git$), ~r(\.gitignore), ~r(README), ~r(assets)]
+  @ignore [~r(\.git$), ~r(\.gitignore), ~r(README), ~r(assets), ~r(exs$)]
 
   def link do
-    IO.puts("I'll link all these configs for you! Let's go:")
+    "I'll link all these configs for you! Let's go:"
+    |> info()
+
     actual_path = Path.expand(".")
 
     case File.stat(@config_path) do
       {:ok, _stat} ->
-        IO.puts("Good, .config folder exists!")
+        "Good, .config folder exists!\n"
+        |> info()
 
       _ ->
-        IO.puts("Well, .config folder doesn't exists... I'll create to you!")
+        "Well, .config folder doesn't exists... I'll create to you!"
+        |> warn()
+
         File.mkdir!(@config_path)
-        IO.puts("Created .config foler on home dir!")
+
+        "Created .config foler on home dir!\n"
+        |> success()
     end
 
     for config <- Path.wildcard("./*", match_dot: true) do
-      existing_path = actual_path <> "/#{config}"
+      config_path = actual_path <> "/#{config}"
 
       cond do
         @ignore |> Enum.filter(&(config =~ &1)) |> length() > 0 ->
           skip(config)
 
         config =~ ~r(^\..+) ->
-          if File.exists?(@home_dir <> "/#{config}") do
-            skip(config)
-          else
-            existing_path
-            |> link_to_home(config)
-
-            IO.puts("Linked #{config}!")
-          end
+          (@home_dir <> "/#{config}")
+          |> link_if_exist(config, config_path, &link_to_home/2)
 
         config == "scripts" ->
-          if File.exists?(@home_dir <> "/#{config}") do
-            skip(config)
-          else
-            existing_path
-            |> link_to_home(config)
-
-            IO.puts("Linked #{config}!")
-          end
+          (@home_dir <> "/#{config}")
+          |> link_if_exist(config, config_path, &link_to_home/2)
 
         config == "xsessions" ->
-          if File.exists?("/usr/share/#{config}") do
-            skip(config)
-          else
+          commands =
             "ln -s #{actual_path}/#{config} /usr/share/#{config}"
             |> String.split(" ")
-            |> root_config()
 
-            IO.puts("Linked #{config}!")
-          end
+          "/usr/share/#{config}"
+          |> link_if_exist(config, &root_config/1, commands: commands)
 
-        config =~ ~r(exs$) ->
-          skip(config)
+        config == "udev_rules" ->
+          slah_position = 11
+
+          Path.wildcard("./#{config}/*")
+          |> Enum.map(fn rule_path -> rule_path |> String.split_at(slah_position) |> elem(1) end)
+          |> Enum.each(fn rule ->
+            commands =
+              "ln -s #{config_path}/#{rule} /etc/udev/rules.d/#{rule}"
+              |> String.split(" ")
+
+            "/etc/udev/rules.d/#{rule}"
+            |> link_if_exist(config, &root_config/1, commands: commands)
+          end)
 
         true ->
-          if File.exists?(@config_path <> "/#{config}") do
-            skip(config)
-          else
-            existing_path
-            |> link_to_dot_config(config)
-
-            IO.puts("Linked #{config}!")
-          end
+          (@config_path <> "/#{config}")
+          |> link_if_exist(config, config_path, &link_to_dot_config/2)
       end
     end
+
+    IO.puts("")
 
     "Linked all! Congratutalions!"
     |> success()
 
     "Please, remember to change ~/.gitconfig file"
-    |> warning()
-  end
-
-  defp link_to_home(existing, config), do: File.ln_s!(existing, @home_dir <> "/#{config}")
-
-  defp link_to_dot_config(existing, config),
-    do: File.ln_s!(existing, @config_path <> "/#{config}")
-
-  defp root_config(commands) do
-    IO.puts("Please, insert your sudo pass:")
-    System.cmd("sudo", ["-A"] ++ commands, into: :io.get_password() |> List.to_string())
-  end
-
-  defp skip(config), do: IO.puts("Skipping #{config}...")
-
-  defmodule Colors do
-    import IO.ANSI, only: [format: 2]
-
-    def success(task) do
-      ["DONE: ", :green, :bold, task, :green]
-      |> format(true)
-      |> IO.puts()
-    end
-
-    def error(reason) do
-      ["ERROR: ", :red, :bold, reason, :red]
-      |> format(true)
-      |> IO.puts()
-    end
-
-    def warn(info) do
-      ["WARNING: ", :yellow, :bold, info, :yellow]
-      |> format(true)
-      |> IO.puts()
-    end
+    |> warn()
   end
 end
 
