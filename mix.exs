4e4d3db0d2b600f5c68f926d631d620ba48e5964
Matheus de Souza Pessanha <matheus_pessanha2001@outlook.com>
2021-03-06 15:57:35 -0300

    added credo and dialyzer

--- mix.exs
+++ mix.exs
@@ -25,6 +25,8 @@ defmodule Dotfiles.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
     ]
   end
 
