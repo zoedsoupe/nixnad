cca34c7c1be69da178df66daba79ce6b13d32850
Matheus de Souza Pessanha <matheus_pessanha2001@outlook.com>
2021-05-03 14:36:08 -0300

    disabled ocaml and rust emacs support

--- home/doom.d/init.el
+++ home/doom.d/init.el
@@ -91,11 +91,11 @@
        ;latex             ; writing papers in Emacs has never been so fun
        lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
-       ocaml             ; an objective camel
+       ;ocaml             ; an objective camel
        org               ; organize your plain life in plain text
        ;;python            ; beautiful is better than ugly
        ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       ;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        ;;scheme            ; a fully conniving family of lisps
        (sh +fish)                ; she sells {ba,z,fi}sh shells on the C xor
