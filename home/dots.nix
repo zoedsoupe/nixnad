9e73fb18e31d140f286a9e2216be21cbec31d3b9
Matheus de Souza Pessanha <matheus_pessanha2001@outlook.com>
2021-05-14 01:03:08 -0300

    full gnome nixos installation

--- home/dots.nix
+++ home/dots.nix
@@ -4,20 +4,8 @@
   home.file = {
     ".iex.exs".text = "IEx.configure(inspect: [limit: :infinity, pretty: true])";
 
-    ".patat.yaml".text = ''
-      wrap: true
-      incrementalLists: true
-      images:
-        backend: auto
-    '';
-
     ".ghci".text = ''
       :set prompt "\ESC[1;34m%s\n\ESC[0;34mλ> \ESC[m"
     '';
-  };
-
-  xdg.configFile.xmobar = {
-    source = ./xmobar;
-    recursive = true;
   };
 }
