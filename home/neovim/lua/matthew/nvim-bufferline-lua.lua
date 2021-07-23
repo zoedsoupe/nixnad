7812c27919af1a7c42fc757a7f833b8d11d36f7d
Zoey <zoey.spessanha@zeetech.io>
2021-07-23 08:15:44 -0300

    generalize direnv layout postgres

--- home/neovim/lua/matthew/nvim-bufferline-lua.lua
+++ home/neovim/lua/matthew/nvim-bufferline-lua.lua
@@ -2,7 +2,5 @@ require'bufferline'.setup {
   options = {
     view = 'multiwindow',
     separator_style = 'slant',
-  },
+  }
 }
--- Colors are taken care of directly in colorscheme
-vim.cmd 'au! BufferlineColors ColorScheme'
