02b260b413ce4057badb6f30c6e25f2c7ced3f30
Matheus de Souza Pessanha <matheus_pessanha2001@outlook.com>
2021-08-01 18:31:41 -0300

    new nvim-tree-lua

--- home/neovim/lua/matthew/nvim-tree-lua.lua
+++ home/neovim/lua/matthew/nvim-tree-lua.lua
@@ -2,16 +2,52 @@ local g = vim.g
 
 vim.cmd 'highlight NvimTreeFolderIcon guibg=blue'
 
-g.nvim_tree_git_hl = 1
-g.nvim_tree_width = 32
-g.nvim_tree_follow = 1
-g.nvim_tree_gitignore = 1
-g.nvim_tree_auto_open = 1
-g.nvim_tree_auto_close = 1
-g.nvim_tree_indent_markers = 1
-g.nvim_tree_highlight_opened_files = 1
-g.nvim_tree_auto_ignore_ft = { 'dashboard' }
-g.nvim_tree_ignore = { '.git', 'node_modules', '_build', '.nix-hex', '.nix-mix', '.postgres' }
+local config = {
+   side = "left",
+    show_icons = {
+      git = 1,
+      folders = 1,
+      files = 1,
+      folder_arrows = 1,
+      tree_width = 30,
+    },
+    ignore = { '.git', 'node_modules', '_build', '.nix-hex', '.nix-mix', '.postgres' },
+    auto_open = 1,
+    auto_close = 1,
+    quit_on_open = 0,
+    follow = 1,
+    hide_dotfiles = 1,
+    git_hl = 1,
+    root_folder_modifier = ":t",
+    tab_open = 0,
+    allow_resize = 1,
+    lsp_diagnostics = 1,
+    auto_ignore_ft = { "startify", "dashboard" },
+    icons = {
+      default = "",
+      symlink = "",
+      git = {
+        unstaged = "",
+        staged = "S",
+        unmerged = "",
+        renamed = "➜",
+        deleted = "",
+        untracked = "U",
+        ignored = "◌",
+      },
+      folder = {
+        default = "",
+        open = "",
+        empty = "",
+        empty_open = "",
+        symlink = "",
+      },
+    }
+}
+
+for opt, val in pairs(config) do
+  g["nvim_tree_" .. opt] = val
+end
 
 vim.api.nvim_set_keymap('', '<C-n>', ':NvimTreeToggle<cr>', { noremap = true })
-vim.api.nvim_set_keymap('', '<C-n>', ':NvimTreeFindFile<cr>', { noremap = true })
+vim.api.nvim_set_keymap('', '<leader>n', ':NvimTreeFindFile<cr>', { noremap = true })
