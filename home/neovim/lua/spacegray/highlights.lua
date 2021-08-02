d833d4c22beca0f849ac7f5114fd7d64d30c2c0d
Matheus de Souza Pessanha <matheus_pessanha2001@outlook.com>
2021-08-01 23:13:41 -0300

    fix gitsigns highlight

--- home/neovim/lua/spacegray/highlights.lua
+++ home/neovim/lua/spacegray/highlights.lua
@@ -94,6 +94,14 @@ local highlights = {
   TabLine = { fg = C.light_gray, bg = C.alt_bg },
   TabLineSel = { fg = C.white, bg = C.alt_bg },
   TabLineFill = { fg = C.white, bg = C.alt_bg },
+
+  -- Git
+  SignAdd = { fg = C.sign_add },
+  SignChange = { fg = C.sign_change },
+  SignDelete = { fg = C.sign_delete },
+  GitSignsAdd = { fg = C.sign_add },
+  GitSignsChange = { fg = C.sign_change },
+  GitSignsDelete = { fg = C.sign_delete },
 }
 
 return highlights
