52938d438164f6779633764c8133545d01f43c7a
Matheus de Souza Pessanha <matheus_pessanha2001@outlook.com>
2021-08-14 13:40:24 -0300

    changefor stable nix channel and use local doom emacs supervised by nix

---
+++ home/emacs.nix
@@ -0,0 +1,40 @@
+{ pkgs, config, lib, ... }:
+
+{
+  home = {
+    sessionPath = [ "${config.xdg.configHome}/emacs/bin" ];
+    sessionVariables = {
+      DOOMDIR = "${config.xdg.configHome}/doom-config";
+      DOOMLOCALDIR = "${config.xdg.configHome}/doom-local";
+    };
+  };
+
+  xdg = {
+    enable = true;
+    configFile = {
+      "doom-config" = {
+        recursive = true;
+        source = ./doom.d;
+      };
+      "emacs" = {
+        source = builtins.fetchGit "https://github.com/hlissner/doom-emacs";
+        onChange = "${pkgs.writeShellScript "doom-change" ''
+          export DOOMDIR="${config.home.sessionVariables.DOOMDIR}"
+          export DOOMLOCALDIR="${config.home.sessionVariables.DOOMLOCALDIR}"
+          if [ ! -d "$DOOMLOCALDIR" ]; then
+            ${config.xdg.configHome}/emacs/bin/doom -y install
+          else
+            ${config.xdg.configHome}/emacs/bin/doom -y sync -u
+          fi
+        ''}";
+      };
+    };
+  };
+
+  programs.emacs.enable = true;
+  services.emacs = {
+    enable = true;
+    client.enable = true;
+    socketActivation.enable = true;
+  };
+}
