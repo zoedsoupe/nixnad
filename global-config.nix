a11874a04e679a091fecbc6b3b5d0da57c280609
Zoey <zoey.spessanha@zeetech.io>
2021-08-18 23:49:26 -0300

    added flake.lock

--- global-config.nix
+++ global-config.nix
@@ -1,7 +1,7 @@
 rec {
   username = "zoedsoupe";
   email = "zoey.spessanha@zeetech.io";
-  selected-desktop-environment = "stumpwm";
+  selected-desktop-environment = "gnome";
   rootPath = "/home/${username}/.dotfiles";
   rooPathNix = rootPath;
   flake = import ./lib/flake { };
