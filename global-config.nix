83ca54f43de81449cf07f444425da69dafde96d4
Zoey <zoey.spessanha@zeetech.io>
2021-08-19 13:41:09 -0300

    fix flakes neovim and go to unstable channel

--- global-config.nix
+++ global-config.nix
@@ -2,7 +2,7 @@ rec {
   username = "zoedsoupe";
   email = "zoey.spessanha@zeetech.io";
   selected-desktop-environment = "gnome";
-  rootPath = "/home/${username}/.dotfiles";
+  rootPath = "/home/${username}/documents/nixnad";
   rooPathNix = rootPath;
   flake = import ./lib/flake { };
 }
--- home/modules/fish.nix
+++ home/modules/fish.nix
@@ -45,7 +45,7 @@ let
     function __direnv_export_eval --on-event fish_prompt;
         begin;
             begin;
-                "/run/current-system/sw/bin/direnv" export fish
+                "/etc/profiles/per-user/zoedsoupe/bin/direnv" export fish
             end 1>| source
         end 2>| egrep -v -e "^direnv: export"
     end;
